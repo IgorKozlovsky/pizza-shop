1790891af08ef4190e2eede4f2a9baf97cd5143d
Игорь Козловский <turanska2004@gmail.com>
2022-04-04 23:37:49 +0300

    Head layout completed

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -3,7 +3,32 @@ import classes from "../../scss/components/_nav-bar.module.scss";
 function NavBar() {
   return (
     <div className={classes.navbar_wrapper}>
-      <div></div>
+      <div className={classes.navbar_bar}>
+        <div className={classes.logo}>
+          <a href="#">
+            <img src={require("../../assets/img/icon.png")} />
+            <span>Курсова піца</span>
+          </a>
+        </div>
+
+        <div className={classes.nav}>
+          <button>Піца</button>
+          <button>Напої</button>
+          <button>Десерти</button>
+          <button>Конструктор</button>
+        </div>
+
+        <button className={classes.bucket}>
+          <div>
+            <span>0</span>
+            <img src={require("../../assets/img/basket.png")} />
+          </div>
+          <div>
+            <span>Замовити</span>
+            <span>00.00</span>
+          </div>
+        </button>
+      </div>
     </div>
   );
 }
