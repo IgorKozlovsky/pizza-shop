f2720ef785b4d38cda26856646cce5fe32131ded
Игорь Козловский <turanska2004@gmail.com>
2022-04-21 20:28:41 +0300

    Finish

--- src/components/form.jsx
+++ src/components/form.jsx
@@ -1,33 +1,65 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { useSelector } from "react-redux";
 
 export default function Form() {
   //prettier-ignore
-  const { register, handleSubmit, formState: { errors } } = useForm();
-  const onSubmit = (data) => console.log(data);
-  console.log(errors);
+  const { items, totalPrice } = useSelector(({ cart }) => cart);
+  //prettier-ignore
 
+  const { register, handleSubmit, formState: { errors } } = useForm();
+  const inpOptions = { required: true, maxLength: 40, minLength: 1 };
+  const onSubmit = (data) => {
+    let text = " \n\n";
+    Object.values(items).map((e) => {
+      return Object.values(e).map((element) =>
+        Object.values(element).map((el) => {
+          for (let i = 0; i < el.length; i++) {
+            //prettier-ignore
+            text = text.concat(`   ${el[i].name}, ${el[i].type}${el[i].size  ? `, ${el[i].size} см` : ""}, ${el[i].price}₴  \n`);
+          }
+        })
+      );
+    });
+    console.log(
+      `
+  //  Замовлення: ${text}
+  //  Cума замовлення: ${totalPrice}₴
+  //  Номер телефону: ${data.tel}
+  //  Адреса: ${data.city}, ${data.road}, ${data.house}, ${data.flat}, ${data.entrance}, ${data.code}, ${data.floor}
+  //  Коментар: ${data.comm}
+  //  Дата та час: ${data.date} ${data.time}
+  //  Оплата: ${data.pay}
+   `
+    );
+  };
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
-      <input type="text" placeholder="First name" {...register("First name", { required: true, maxLength: 80 })} />
-      <input type="text" placeholder="Last name" {...register("Last name", { required: true, maxLength: 100 })} />
-      <input type="text" placeholder="Email" {...register("Email", { required: true, pattern: /^\S+@\S+$/i })} />
-      <input
-        type="tel"
-        placeholder="Mobile number"
-        {...register("Mobile number", { required: true, minLength: 6, maxLength: 12 })}
-      />
-      <select {...register("Title", { required: true })}>
-        <option value="Mr">Mr</option>
-        <option value="Mrs">Mrs</option>
-        <option value="Miss">Miss</option>
-        <option value="Dr">Dr</option>
+      <h2>Контакти:</h2>
+      <input {...register("tel", { required: true })} type="tel" placeholder="Телефон"></input>
+      <h2>Адреса:</h2>
+      <article>
+        <input {...register("city", inpOptions)} type="text" placeholder="Місто"></input>
+        <input {...register("road", inpOptions)} type="text" placeholder="Вулиця"></input>
+        <input {...register("house", inpOptions)} type="text" placeholder="Будинок"></input>
+        <input {...register("flat", inpOptions)} type="number" placeholder="Квартира"></input>
+        <input {...register("entrance", inpOptions)} type="text" placeholder="Підїзд"></input>
+        <input {...register("code", inpOptions)} type="text" placeholder="Код"></input>
+        <input {...register("floor", inpOptions)} type="number" placeholder="Поверх"></input>
+      </article>
+      <textarea {...register("comm")} rows="5" cols="37" placeholder="Коментар"></textarea>
+      <h2>Дата та час:</h2>
+      <input {...register("date", inpOptions)} type="text" placeholder="Дата"></input>
+      <input {...register("time", inpOptions)} type="text" placeholder="Час"></input>
+      <h2>Оплата:</h2>
+      <select {...register("pay")}>
+        <option value="Готівка">Готівка</option>
+        <option value="Карткою кур'єру">Карткою кур'єру</option>
       </select>
-
-      <input {...register("Developer", { required: true })} type="radio" value="Yes" />
-      <input {...register("Developer", { required: true })} type="radio" value="No" />
-
-      <input type="submit" />
+      <div>
+        <p>Усього: {totalPrice}.00 ₴</p>
+        <input className="inp_submt" type="submit" value="Заказати" />
+      </div>
     </form>
   );
 }
--- src/pages/cart.jsx
+++ src/pages/cart.jsx
@@ -1,74 +1,19 @@
 import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
-import { useForm } from "react-hook-form";
 import CartItemsWrapper from "../components/cart-item-wrapper";
+import Form from "../components/form";
 
 function Cart() {
-  //prettier-ignore
-  const {data, handleSubmit, formState: { errors }} = useForm();
-  const { items, totalPrice, totalCount } = useSelector(({ cart }) => cart);
+  const { totalPrice, totalCount } = useSelector(({ cart }) => cart);
   const { pizza, drinks, desserts } = useSelector(({ cart }) => cart.items);
-  console.log(errors);
-  const onCollectData = () => {
-    let text = " \n\n";
-    Object.values(items).map((e) => {
-      return Object.values(e).map((element) =>
-        Object.values(element).map((el) => {
-          for (let i = 0; i < el.length; i++) {
-            //prettier-ignore
-            text = text.concat(`        ${el[i].name}, ${el[i].type}${el[i].size  ? `, ${el[i].size} см` : ""}, ${el[i].price}₴  \n`);
-          }
-        })
-      );
-    });
-    console.log(
-      `
-    //  Замовлення: ${text}
-    //  Cума замовлення: ${totalPrice}
-    //  Номер телефону: ${"dd"}
-    //  Адреса: ${"dd"}
-    //  Коментар: ${"dd"}
-    //  Дата та час: ${"dd"}
-    //  Оплата: ${"dd"}
-     `
-    );
-  };
-  const onSubmit = (data) => console.log(data);
-  const inpOptions = { required: true, maxLength: 40, minLength: 1 };
+
   return (
     <div className="cart_wrapper">
       {totalCount ? (
         <>
           <main className="cart_main">
-            <form onSubmit={handleSubmit(onSubmit)}>
-              <h2>Контакти:</h2>
-              <input {...data("tel", { required: true })} type="tel" placeholder="Телефон"></input>
-              <h2>Адреса:</h2>
-              <article>
-                <input {...data("city", inpOptions)} type="text" placeholder="Місто"></input>
-                <input {...data("road", inpOptions)} type="text" placeholder="Вулиця"></input>
-                <input {...data("house", inpOptions)} type="text" placeholder="Будинок"></input>
-                <input {...data("flat", inpOptions)} type="number" placeholder="Квартира"></input>
-                <input {...data("entrance", inpOptions)} type="text" placeholder="Підїзд"></input>
-                <input {...data("code", inpOptions)} type="text" placeholder="Код"></input>
-                <input {...data("floor", inpOptions)} type="number" placeholder="Поверх"></input>
-              </article>
-              <textarea {...data("comm")} rows="5" cols="37" placeholder="Коментар"></textarea>
-              <h2>Дата та час:</h2>
-              <input {...data("date", inpOptions)} type="text" placeholder="Дата"></input>
-              <input {...data("time", inpOptions)} type="text" placeholder="Час"></input>
-              <h2>Оплата:</h2>
-              <select {...data("pay")}>
-                <option value="Готівка">Готівка</option>
-                <option value="Карткою кур'єру">Карткою кур'єру</option>
-              </select>
-              {/*onClick={onCollectData}*/}
-              <div>
-                <p>Усьго: {totalPrice}.00 ₴</p>
-                <input type="submit">Заказати</input>
-              </div>
-            </form>
+            <Form />
           </main>
           <section className="cart_side_wrapper">
             <article className="cart_side" id="cart_side_scroll">
